24b40c118ff33dddffe9698266af67164dcb6fbf
Tarun Kumar <119564471+Tarun12222850@users.noreply.github.com>
2023-10-12 01:31:40 +0530

    Add files via upload

---
+++ Lecture11-I.cpp
@@ -0,0 +1,9 @@
+// Lecture 11 I - Address of Elements in C++ Pt 1
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "The size of int is : " << sizeof(int) << endl;
+    cout << "The size of long long int is : " << sizeof(long long int) << endl;
+    return 0;
+}
