ce3bfdb1c1ae8735af51545fd73af4517af5d506
Tarun Kumar <119564471+Tarun12222850@users.noreply.github.com>
2023-10-12 01:37:58 +0530

    Update Lecture11-I.cpp

--- Lecture11-I.cpp
+++ Lecture11-I.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main(){
     cout << "The size of int is : " << sizeof(int) << endl;
+    cout << "The size of long int is : " << sizeof(long int) << endl;
     cout << "The size of long long int is : " << sizeof(long long int) << endl;
+    cout << "The size of float is : " << sizeof(float) << endl;
+    cout << "The size of double is : " << sizeof(double) << endl;
+    cout << "The size of char is : " << sizeof(char) << endl;
     return 0;
 }
